9b10981a4925f04ed5466849cd3d54fb9b1a993a
Alexey Alexandrov <AleksandrovAlexey0929@yandex.ru>
2024-05-15 11:58:51 +0300

    implit gaps` amount can be reduced by increasing window

--- back/src/main/java/spring/buttowski/diploma/models/Coordinate.java
+++ back/src/main/java/spring/buttowski/diploma/models/Coordinate.java
@@ -1,7 +1,9 @@
 package spring.buttowski.diploma.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -12,8 +14,11 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Coordinate {
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime time;
     private int burnersNum;
     private Double steamCapacity;
+    private Double masutPresure;
+    private Double masutConsumption;
 
 }
--- back/src/main/java/spring/buttowski/diploma/services/DataService.java
+++ back/src/main/java/spring/buttowski/diploma/services/DataService.java
@@ -1,5 +1,6 @@
 package spring.buttowski.diploma.services;
 
+import spring.buttowski.diploma.controllers.DataController;
 import spring.buttowski.diploma.models.Coordinate;
 import spring.buttowski.diploma.models.Data;
 import spring.buttowski.diploma.repositories.DataRepository;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -27,61 +29,67 @@ public class DataService {
         this.productListParser = productListParser;
     }
 
-    public List<Coordinate> getData(LocalDateTime dateFrom, LocalDateTime dateTo, boolean additional) {
-        List<Data> dataList = dataRepository.findDataByTimeBetween(dateFrom, dateTo);
-        approximate(dataList, 50);
-        List<Coordinate> coordinates = burnerNumberDetermination.getBurnersAmountByClusterization(dataList, additional);
-
-        int previousBurnersNum;
-        int currentBurnersNum;
-        int counter = 0;
-        LocalDateTime previousTime;
-
-        Iterator<Coordinate> iterator = coordinates.iterator();
-        Coordinate first = iterator.next();
-        previousBurnersNum = first.getBurnersNum() - 1;
-        previousTime = first.getTime();
-
-        while (iterator.hasNext()) {
-            Coordinate coordinate = iterator.next();
-            currentBurnersNum = coordinate.getBurnersNum();
-            while (currentBurnersNum == previousBurnersNum) {
-                if (!iterator.hasNext()) break;
-                coordinate = iterator.next();
-                currentBurnersNum = coordinate.getBurnersNum();
-                counter++;
-            }
-            if (counter < 60) {
-                System.out.println(previousTime + " - " + coordinate.getTime().minusMinutes(1));
-            }
-            counter = 0;
-            previousBurnersNum = currentBurnersNum;
-            previousTime = coordinate.getTime();
+    public List<Coordinate> getData(LocalDateTime dateFrom, LocalDateTime dateTo, boolean showCapacity) {
+        if (dateFrom.equals(dateTo)) {
+            return Collections.emptyList();
         }
+        //Находим все нужные нам значения параметров работы за определённый промежуток времени
+        List<Data> dataList = dataRepository.findDataByTimeBetween(dateFrom, dateTo);
+
+        //"Сглаживаем" данные методом кользящего среднего
+        movingAverage(dataList, 100);
+
+        //Считаем кол-во включенных горелок для каждого момента времени
+        List<Coordinate> coordinates = BurnerNumberDetermination.getBurnersAmountByClusterization(dataList, showCapacity);
+
+        //Печатаем все промежутки времени
+        countGaps(coordinates);
+
         return coordinates;
     }
 
-    private void approximate(List<Data> dataList, int border) {
+    public static void countGaps(List<Coordinate> coordinates) {
+        System.out.println("-------------");
+        Iterator<Coordinate> iterator = coordinates.iterator();
+        Coordinate gapStart, next;
+        int timeCounter;
+        int implicitGapsCounter = 0;
+        while (iterator.hasNext()) {
+            for (gapStart = iterator.next(), next = iterator.next(), timeCounter = 0;
+                 iterator.hasNext() && gapStart.getBurnersNum() == next.getBurnersNum();
+                 next = iterator.next()) {
+                timeCounter++;
+            }
+            String alarm = "";
+            if (timeCounter < 60) {
+                implicitGapsCounter++;
+                alarm = " <60 минут!";
+            }
+            System.out.println(gapStart.getTime().format(DataController.formatter) +
+                    " - " + next.getTime().minusMinutes(1).format(DataController.formatter)
+                    + " " + gapStart.getBurnersNum() + " " + alarm);
+        }
+        System.out.println("-------------(Implicit gaps' amount - " + implicitGapsCounter + ")");
+//        System.out.println(implicitGapsCounter);
+    }
+
+    //TODO Можно оптимизировать путём использования суммы с прошлой итерации
+    public static void movingAverage(List<Data> dataList, int border) {
         Data[] dataArray = dataList.toArray(new Data[0]);
         double sumSteamCapacity = 0.0;
         double sumMasutPresure = 0.0;
         double sumMasutConsumption = 0.0;
         for (int outerIndex = 0; outerIndex < dataList.size(); outerIndex++) {
-            for (int innerIndex = Math.max(0, outerIndex - border);
-                 innerIndex < Math.min(dataList.size(), outerIndex + border);
+            int from = Math.max(0, outerIndex - border);
+            int to = Math.min(dataList.size() - 1, outerIndex + border);
+            for (int innerIndex = from;
+                 innerIndex <= to;
                  innerIndex++) {
                 sumSteamCapacity += dataArray[innerIndex].getSteamCapacity();
                 sumMasutConsumption += dataArray[innerIndex].getMasutConsumtion();
                 sumMasutPresure += dataArray[innerIndex].getMasutPresure();
             }
-            //equals outerIndex in case it between 0 and 30
-            int leftQuantity = Math.min(outerIndex, border);
-            //equals outerIndex in case it between dataList.size()-30 and dataList.size()
-            int rightQuantity = border;
-            if (outerIndex > dataList.size() - border) {
-                rightQuantity = dataList.size() - outerIndex;
-            }
-            int quantity = leftQuantity + rightQuantity;
+            int quantity = to - from + 1;
             dataList.get(outerIndex).setSteamCapacity(sumSteamCapacity / quantity);
             dataList.get(outerIndex).setMasutPresure(sumMasutPresure / quantity);
             dataList.get(outerIndex).setMasutConsumtion(sumMasutConsumption / quantity);
